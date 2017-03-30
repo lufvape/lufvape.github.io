89c355d1c52a2435e1280b856c8b8c09b0e6c343
lfvargas1746 <lf.vargas1746>
2017-03-29 21:58:36 -0500

    nuevo
    todo again

---
+++ js/modal.js
@@ -0,0 +1,15 @@
+/**
+ * Created by luisfvargas on 22/06/16.
+ */
+
+$(document).ready(function(){
+
+    $('.modal-trigger').leanModal();
+
+    $('.slider').slider({
+        full_width: true,
+    });
+
+});
+
+
