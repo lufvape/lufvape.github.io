504c4d6d9d66ed697a72714443206faaa305a0f5
lfvargas1746 <lf.vargas1746>
2017-04-06 10:47:15 -0500

    proyectos
    w

---
+++ js/appes.js
@@ -0,0 +1,60 @@
+/**
+ * Created by luisfvargas on 6/04/17.
+ */
+
+var app = angular.module('plunkeres', ['ngRoute']);
+
+app.config(function($routeProvider) {
+
+        $routeProvider.when('/main', {
+            templateUrl: "maines.html",
+            controller: "mainController"
+        }).when('/detail/:productName', {
+            templateUrl: "detailses.html",
+            controller: "detailsController"
+        }).otherwise({
+            redirectTo: "/main"
+        });
+    })
+    .controller("mainController", function($scope, dataService) {
+        $scope.store = {}; //.products
+
+        $scope.store.products = dataService.getProducts();
+    })
+    .controller("detailsController", function($scope, $routeParams, dataService) {
+
+
+        $scope.product = dataService.getProductAt($routeParams.productName);
+
+    });
+
+
+angular.module("plunkeres").service("dataService", function(filterFilter) {
+    var items = [{
+        "name": "Nexus 5",
+        "size": "4.95 inches",
+        "camera": "8 megapixels"
+    }, {
+        "name": "Nexus 6",
+        "size": "6.0 inches",
+        "camera": "13 megapixels"
+    }, {
+        "name": "Nexus 6",
+        "size": "6.0 inches",
+        "camera": "13 megapixels"
+    },{
+        "name": "Nexus 6",
+        "size": "6.0 inches",
+        "camera": "13 megapixels"
+    }];
+
+    this.getProducts = function() {
+        return items;
+    };
+    this.getProductAt = function(_name) {
+        this.getProducts();
+        return filterFilter(items, {
+            name: _name
+        })[0];
+    };
+});
