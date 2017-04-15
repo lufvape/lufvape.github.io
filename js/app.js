2f3f7ccabd27441dee71cb0b4177687acc6c30e0
lfvargas1746 <lf.vargas1746>
2017-04-15 11:32:07 -0500

    ingles
    qweqwe

--- js/app.js
+++ js/app.js
@@ -30,10 +30,10 @@ angular.module("plunker").service("dataService", function(filterFilter) {
         "client": "MSMc Arquitectos",
         "place": "Ibagué",
         "date": "2016",
-        "description":"La firma de arquitectos MSMc Arquitectos me confió la realización de su portafolio web. El proyecto comenzó con entender las necesidades de los arquitectos y el nicho al que dirigían su página" +
-        "mediante una serie de reuniones entre las partes. El siguiente paso fue realizar el estudio de los usuarios, esto mediante unas pocas entrevistas y la creación de personas y mapas de usuario." +
-        " Luego se realizaron los primeros bocetos de las páginas que se requerían, teniendo en cuenta hacer las interacciones simples.El proceso fue incremental, comenzando desde lo básico con papel y lápiz hasta llegar a prototipos de alta fidelidad." +
-        " Finalmente se decidieron las herramientas y marcos de trabajo a utilizar, se trabajó con materializeCSS y Jquery para manejar las interacciones.",
+        "description":"The studio MSMc Arquitectos trust me their new web portfolio. The project started with discovering the needs of both the arquitects and their target, this was done by a series of meetings with stakeholders." +
+        "The next step was to conduct interviews to users in order to develop some personas and journey maps that help to understand the target market and their needs." +
+        " Taking into account that simple interaction across the page must be made the first sketches were done. The process was incremental, from simple paper to high fidelity prototypes." +
+        " Finally, the tools were chosen. Materializecss and Jquery were used for interactions and look of the page.",
         "image": "../img/msmc.svg",
         "define": "../img/msmc1.png",
         "understand": "../img/msmc2.png",
@@ -48,10 +48,10 @@ angular.module("plunker").service("dataService", function(filterFilter) {
         "client": "Marfil",
         "place": "Ibagué",
         "date": "2016",
-        "description":"Marfil es una empresa familiar ibaguereña que quería comenzar a tener presencia en la red mediante una plataforma para que sus clientes pudieran ordenar sus productos en linea. " +
-        "Todo comenzó con una reunión con los dueños, mediante la cual se establecieron las prioridades de la aplicación. El siguiente paso fue comenzar a construir los bocetos preliminares con ayuda de los mismos empleados de la empresa y clientes." +
-        "La funcionalidad se fue desarrollando y probando iterativamente con los empleados, la empresa y algunos usuarios, con esto se lograron hacer modificaciones y tener un sistema agradable al uso. " +
-        "Para el prototipado final e implementación de la aplicación web se utilizaron Angular, Firebase y Bootstrap.",
+        "description":"Marfil is a familly business from Ibagué. They wanted to start an online presence with a web app that allow users to order they products online. " +
+        "Everything started with a meeting with the owners to establish the priorities of the application. The next step was to co-create with all stakeholders in order to develop the first sketches for the app." +
+        "Functionality was iteratively developed and test with both users and business, by doing this, interactions and ease of use was achived. " +
+        "Final implementation was made with Angular, Firebase and Bootstrap.",
         "image": "../img/marfil.svg",
         "define": "../img/marfil1.jpg",
         "understand": "../img/marfil2.jpg",
@@ -66,10 +66,10 @@ angular.module("plunker").service("dataService", function(filterFilter) {
         "client": "IDEO(MOOC)",
         "place": "Bogotá",
         "date": "2016",
-        "description":"En el marco del curso de diseño centrado en el usuario dictado de manera virtual por IDEO formamos un grupo de trabajo para dar solución al problema de desperdicio de comida. Nuestro nicho de mercado fueron solteros y recién casados. " +
-        "Como primera medida se diseñaron distintos tipos de entrevistas, una para expertos como chefs y amas de casa, y otra para el grupo al que queríamos intervenir, las entrevistas se realizaron a diferentes personas y se obtuvieron datos relevantes e interesantes sobre el problema " +
-        "En la fase de ideación se tomaron en cuenta todas las ideas que pudieran llegar a la mente, sin restricciones se pudo llegar a conceptos interesantes y que podían ayudar a mejorar tanto la visión del grupo objetivo como el nivel de responsabilidad hacia el problema " +
-        "Finalmente se realizó un prototipo inicial que consistía en una apalicación la cual tenía el principio de una red social que lograba integrarse con su ambiente y un kit para la cocina. ",
+        "description":"At the online course of human centered design by IDEO we formed a team to give a solution to the food waste problem in our community. Focusing on singles and just married couples. " +
+        "At first we designed different types of interviews, one for experts on the field and one for the target market, by conducting the interviews, relevant and interesting data was obtained." +
+        "Ideation was about giving a chance to any idea, without restrictions interesting concepts arrived, and a clear vision about the niche and the responsability of the problem in hand. " +
+        "Finally a first protitype was made. It consist on a mobile app that connects with smart kitchen products to serve as a social network of waste. A kitchen kit was made in order to have an offline experience. ",
         "image": "../img/wastel.svg",
         "define": "../img/hcd1.png",
         "understand": "../img/hcd2.png",
@@ -84,11 +84,10 @@ angular.module("plunker").service("dataService", function(filterFilter) {
         "client": "Uniandes(final project)",
         "place": "Bogotá",
         "date": "2015",
-        "description":"El proyecto SFIMC (Sistematización fácil e intuitiva para Mipymes Colombianas) busca apoyar a las MiPymes colombianas por medio del desarrollo de una plataforma que facilite la sistematización de los procesos básicos de todo tipo de empresa." +
-        " Esta plataforma debe ser economica, facil de instalar y fácil de usar, por lo cual se usan herramientas tecnológicas de software libre y se entregan manuales claros y concisos para que una persona con pocas bases de conocimiento en tecnología pueda desarrollar" +
-        " dichas actividades. Con la iniciativa SFIMC se busca apoyar a los pequeños empresarios y emprendedores para que manejen sus transacciones, automaticen sus procesos de negocio, y controlen de manera fácil y efectiva su negocio sin incurrir en costos que posiblemente" +
-        " no puedan asumir. Adicional a esto es necesario mostrar y hacer entender a las pequeñas organizaciones que las TIC’s no son un gasto, son una inversión, que con cifras tangibles reales, una implementación correcta y un determinado tiempo se recupera lo invertido, y" +
-        " que como consecuencia de esto aumenta la productividad de la empresa (competitividad).",
+        "description":"SFIMC (Sistematización fácil e intuitiva para Mipymes Colombianas) project was made to help colombian SME's by developing a module that enhance the automation of basic business processes." +
+        " This platform must be easy to install and use and cheap, that's why open source projects were chosen. Step by step manuals are also delivered so a person with basic IT knowledge can set up the system." +
+        " With SFIMC initiative the goal is to help owners of SME's and entrepreneurs to manage their transactions, automate their business processes, and effectively control their business with no need to spend a lot of money" +
+        " . Additionally the project intent to show small organizations that IT is not an expense but an investment, that manage in the right way and well implemented is only a matter of time to enhance productivity and get a return on investment.",
         "image": "../img/sfimc.svg",
         "define": "../img/sfimc1.png",
         "understand": "../img/sfimc2.png",
@@ -103,12 +102,12 @@ angular.module("plunker").service("dataService", function(filterFilter) {
         "client": "Uniandes(final project)",
         "place": "Bogotá",
         "date": "2015",
-        "description":"Algunos claustros han aprovechado muy bien las tecnologías de información, hasta el punto de comunicarse y discutir con estudiantes en diferentes partes del mundo, para compartir sus trabajos y experiencias; pero esto es sólo un ejemplo" +
-        " de lo que se puede hacer con la tecnología, se debe buscar una forma más eficiente de vincularla a los procesos de enseñanza-aprendizaje en los colegios. Este es el objetivo de este trabajo: Brindar a estudiantes y docentes de educación básica primaria," +
-        " la descripción de algunas actividades mediante la cual se logre una mejor comprensión del proceso de diseño en ingeniería y su relación con la tecnología, con el fin de generar mayor interés en las áreas de STEM y promover algunas competencias STEM " +
-        "Uno de los aspectos de interés en la última década para este tipo de labor es la robótica, con una gran cantidad de artefactos y posibilidades de dónde elegir; para el caso de estudio actual se trabajará sobre cubelets, " +
-        "Para incluir de forma interesante los robots dentro del entorno educativo se utiliza el marco Understanding by Design(Jay McTighe y Grant Wiggins) en el diseño curricular de la acividad. Esta aproximación busca que el desarrollo de currículos sea de forma descendente" +
-        " desde los objetivos hacia las actividades, teniendo  los resultados finales como el elemento importante, medidos en las capacidades, habilidades, y conocimientos que los estudiantes deben construir para el final de una sesión de trabajo o de la secuencia completa.",
+        "description":"Some schools made a great use of information technology, to the point to connect and discuss with students arround the world, to share works and experiences; but this is just an example of what can be made with IT." +
+        " But it must be a better and more efficient way to bind technology with the process of teaching and learning in schools. An that's the main objective of this work: To give students and teachers of elementary schools," +
+        " the description of some activities by which they get a better knowledge of the engineering design process and it relation to technology, ending with a genuine interest in STEM." +
+        "Robotics have been one of the major influences in the last decade for IT literacy, with a great variety of products to choose from; for this specific project the robots are cubelets from modular robotics. " +
+        "To include robots in class in an interesting way, the framework of work was Understanding by Design(Jay McTighe y Grant Wiggins) for designing the activities. This framework looks for designing the curriculum in a descending way, form objectives to activities, " +
+        " and having the final result as the most important aspect, measured by the skills and knowledge that students must develop at the end of the work session..",
         "image": "../img/ubd.svg",
         "define": "../img/ubd1.png",
         "understand": "../img/ubd2.png",
